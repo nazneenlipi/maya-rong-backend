5c2e744f7a69cd43ea2c64ca31d9c26eecca21b4
nazneenlipi <lipinazneen65@gmail.com>
2025-12-08 00:30:33 +0600

    cors updte

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,13 @@ import config from "./config";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://maya-rong-frontend-v7iw.vercel.app", "http://localhost:3000"], 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/v1/businesses", businessRoutes);
